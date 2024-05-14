ea7d0eeb335883b5b3985f61e225797b3e52229c
ByeongGyu An <an622911@gmail.com>
2024-05-14 20:37:35 +0900

    Fix : UserRoutineCollectionRepository 의 쿼리 파라미터 수정
     @Param 어노테이션을 사용하여 String 파라미터를 받도록 indScrappedRoutineIdByUserEmail과 findScrappedRoutineIdByUserNickname 메서드의 쿼리 파라미터를 수정

--- src/main/java/com/ogjg/daitgym/routine/repository/UserRoutineCollectionRepository.java
+++ src/main/java/com/ogjg/daitgym/routine/repository/UserRoutineCollectionRepository.java
@@ -21,10 +21,10 @@ public interface UserRoutineCollectionRepository extends JpaRepository<UserRouti
     long countByRoutineId(@Param("routineId") Long routineId);
 
     @Query("SELECT urc.routine.id FROM UserRoutineCollection urc WHERE urc.user.email = :email")
-    Set<Long> findScrappedRoutineIdByUserEmail(String email);
+    Set<Long> findScrappedRoutineIdByUserEmail(@Param("email") String email);
 
     @Query("SELECT urc.routine.id FROM UserRoutineCollection urc WHERE urc.user.nickname = :nickname")
-    Set<Long> findScrappedRoutineIdByUserNickname(String nickname);
+    Set<Long> findScrappedRoutineIdByUserNickname(@Param("nickname") String nickname);
 
     boolean existsByUserEmailAndRoutineId(String userEmail, Long routineId);
 }
